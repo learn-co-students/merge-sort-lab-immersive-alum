b267d9876017f8a6008717553388bde0de951b70
Mike <zluo@alumni.berklee.edu>
2017-09-10 23:09:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const findMinAndRemoveSorted = (array) => array.shift()
 
+function merge(firstHalf, secondHalf) {
+  let merged = []
+  while (firstHalf.length > 0 && secondHalf.length > 0) {
+    if (firstHalf[0] < secondHalf[0]) {
+      merged.push(findMinAndRemoveSorted(firstHalf))
+    } else {
+      merged.push(findMinAndRemoveSorted(secondHalf))
+    }
+  }
+  return merged.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array) {
+  let midPoint = Math.floor(array.length / 2)
+  let firstHalf = array.slice(0, midPoint)
+  let secondHalf = array.slice(midPoint, array.length)
+  return array.length < 2 ? array : merge(mergeSort(firstHalf), mergeSort(secondHalf))
+}
