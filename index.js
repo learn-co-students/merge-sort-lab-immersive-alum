1f6974c44828dd46675632b68c2a5f0b918c9554
Zali Perlow <zali.perlow@flatironschool.com>
2017-12-18 14:42:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function findMinAndRemoveSorted(arr) {
+  return arr.shift()
+}
 
+function returnLowest(arr1, arr2) {
+  let firstEl = arr1[0]
+  let secondEl = arr2[0]
+
+  if (firstEl < secondEl) {
+    return findMinAndRemoveSorted(arr1)
+  }
+  return findMinAndRemoveSorted(arr2)
+}
+
+function merge(arr1, arr2) {
+  let sortedArr = []
+  while (arr1.length && arr2.length) {
+    sortedArr.push(returnLowest(arr1, arr2))
+  }
+  // One of arr1 or arr2 is empty. Just concat both since the empty one will have no effect.
+  return sortedArr.concat(arr1).concat(arr2)
+}
+
+function mergeSort(arr) {
+  let middle = arr.length / 2
+  let firstPart = arr.slice(0, middle)
+  let secondPart = arr.slice(middle)
+
+  // Basecase for recursive algorithm
+  if (arr.length < 2) return arr
+  return merge(mergeSort(firstPart), mergeSort(secondPart))
+}
