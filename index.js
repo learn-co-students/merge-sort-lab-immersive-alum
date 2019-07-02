a84a8fb77ed8b54eda99fa96a90da857ce5a7a40
zellfrey <linzell.godfrey@live.co.uk>
2019-07-02 12:47:05 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+function findMinAndRemoveSorted(array){
+    return array.shift()
+}
+
+function merge(array1, array2){
+    let sortedArray = [];
+    while((array1.length != 0) && (array2.length != 0)){
+            if(array1[0] < array2[0]){
+                sortedArray.push(findMinAndRemoveSorted(array1))
+            }else{
+                sortedArray.push(findMinAndRemoveSorted(array2))
+            }
+    }
+    return sortedArray.concat(array1).concat(array2)
+}
+
+function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let sorted = [];
+  
+    if(array.length < 2){
+      return array
+    } else {
+      sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sorted
+  }
